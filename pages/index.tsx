0a851ad7c5438aea8e95973fcd158915c19d2b3f
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-10-05 18:03:04 +0900

    Create layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
-function View() {
-  return <div>Hello Next.js</div>;
-}
+export default () => (
+  <div className="d-md-flex" style={{ minHeight: "100vh" }}>
+    <div
+      className="col-xl-3 col-lg-4 col-md-5 col-sm-12 col-12 px-3 py-4"
+      style={{ backgroundColor: "#2f363d" }}
+    >
+      Side Bar
+    </div>
 
-export default View;
+    <div className="col-xl-9 col-lg-8 col-md-7 col-sm-12 col-12 px-3 py-4">
+      Contents
+    </div>
+  </div>
+);
