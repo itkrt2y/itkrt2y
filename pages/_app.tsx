1a90c9ce90b4e3c6e52bcd04844791c0a3a0315f
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-10-07 14:36:41 +0900

    White twitter logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -69,7 +69,7 @@ export default class extends App {
                       data-name="Logo — FIXED"
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 400 400"
-                      fill="#1da1f2"
+                      fill="#ffffff"
                       aria-label="Twitter"
                       style={{ marginLeft: -5 }}
                     >
