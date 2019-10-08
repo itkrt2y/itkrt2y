526c830381084796b7f352cedb21867d199f3bb7
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-10-08 22:11:50 +0900

    Create a This site page

--- pages/this-site.tsx
+++ pages/this-site.tsx
@@ -2,6 +2,33 @@ import Head from "next/head";
 import Nav from "../components/Nav";
 import { title } from "../lib/header";
 
+const usingTechs = [
+  {
+    name: "Next.js",
+    url: "https://nextjs.org/"
+  },
+  {
+    name: "Docker",
+    url: "https://docker.com/"
+  },
+  {
+    name: "Google Cloud Run",
+    url: "https://cloud.google.com/run/"
+  }
+];
+
+const TechList = () => (
+  <ul className="pl-4 f3">
+    {usingTechs.map(tech => (
+      <li className="mb-2" key={tech.name}>
+        <a href={tech.url} className="text-white">
+          {tech.name}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
 export default () => (
   <>
     <Head>
@@ -11,7 +38,8 @@ export default () => (
     <Nav selected="This Site" />
 
     <div className="px-3">
-      <h2>🚧 Under Construction 🚧</h2>
+      <h2 className="border-bottom pb-2 mb-4">This site is built with</h2>
+      <TechList />
     </div>
   </>
 );
