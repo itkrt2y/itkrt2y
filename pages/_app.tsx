0160b05289dd1024803d697e9d03556c03ca4c67
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2019-10-18 23:35:10 +0900

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,6 +44,32 @@ export default class extends App {
       <>
         <Head>
           <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/images/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#41297e" />
+          <meta name="theme-color" content="#ffffff" />
+
+          <link
             href="https://unpkg.com/primer@11.0.0/build/build.css"
             rel="stylesheet"
           />
