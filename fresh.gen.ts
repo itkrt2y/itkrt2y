441bbda0386607af91b4559c2a98f42cd51845ab
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2022-07-02 19:48:40 +0900

    Remove an unused component

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,6 @@ import * as $0 from "./routes/blog.tsx";
 import * as $1 from "./routes/experience.tsx";
 import * as $2 from "./routes/index.tsx";
 import * as $3 from "./routes/this-site.tsx";
-import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
@@ -15,9 +14,7 @@ const manifest = {
     "./routes/index.tsx": $2,
     "./routes/this-site.tsx": $3,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 };
 
