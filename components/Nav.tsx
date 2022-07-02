783603f2fb781531ac874d07980d0cf7848a49f7
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2022-07-03 00:59:16 +0900

    Fix nav texts

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,14 +3,14 @@ import { h, type FunctionComponent } from "preact";
 import { tw } from "@twind";
 import { ExternalIcon } from "./icons.tsx";
 
-type NavItems = "Blog" | "Experience" | "Skills" | "This Site" | "Gist";
+type NavItems = "BLOG" | "EXPERIENCE" | "SKILLS" | "THIS SITE" | "Gist";
 
 export const Nav: FunctionComponent<{ url: URL }> = ({ url }) => (
   <nav class={tw`flex w-full mb-10`} aria-label="Navigation">
-    <NavItem url={url} name="Skills" href="/" />
-    <NavItem url={url} name="Experience" href="/experience" />
-    <NavItem url={url} name="Blog" href="/blog" />
-    <NavItem url={url} name="This Site" href="/this-site" />
+    <NavItem url={url} name="SKILLS" href="/" />
+    <NavItem url={url} name="EXPERIENCE" href="/experience" />
+    <NavItem url={url} name="BLOG" href="/blog" />
+    <NavItem url={url} name="THIS SITE" href="/this-site" />
     <NavItem
       url={url}
       name="Gist"
@@ -33,7 +33,7 @@ const NavItem = ({
 }) => (
   <a
     href={href}
-    class={tw`flex-1 flex gap-1 items-center justify-center text-white text-sm whitespace-nowrap p-3 border-b-2 hover:bg-gray-700 ${
+    class={tw`flex-1 flex gap-1 items-center justify-center text-white text-sm tracking-wider whitespace-nowrap p-3 border-b-2 hover:bg-gray-700 ${
       url.pathname === href
         ? "border-yellow-600 bg-gray-700"
         : "border-gray-600"
